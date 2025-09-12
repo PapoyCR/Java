6b6f30d98434a7f3c762f900c435c4f8f28757e2
PapoyCR <leonardoc71@nycstudents.net>
2025-09-12 16:13:12 +0000

    w

---
+++ Java-3-Activity/Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+   System.out.println("Hi");
+
+   System.out.println("Leonardo Caragliano");
+
+   System.out.println("Leonardo Caragliano, I live on Sesame Street"); 
+
+   System.out.println("Leonardo Caragliano is my name.\nI live on Sesame Street.\nThis is all in one line of code.");
+  }
+
+  
+}
